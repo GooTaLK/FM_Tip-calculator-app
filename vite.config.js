52bc88b868914ee3af7d985bfb931324db8b2579
GootaLK <lokosmen1235@gmail.com>
2022-06-27 15:00:36 -0500

    Feat: Add autoprefixer package

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import postcssNesting from 'postcss-nesting'
+import autoprefixer from 'autoprefixer'
 
 export default defineConfig({
   css: {
     postcss: {
-      plugins: [postcssNesting()],
+      plugins: [postcssNesting, autoprefixer],
     },
   },
 })
